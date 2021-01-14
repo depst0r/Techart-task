2236759be9ee4f3f2e30322b756f721448c9ee26
depst0r <depstor90@gmail.com>
2021-01-14 18:39:03 +0300

    cse3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,84 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { building } from './Redux/Actions/actions'
+import { building, height, material } from './Redux/Actions/actions'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 export const App = () => {
 
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(3)
   const [checkedValue, setCeckedValue] = useState(String)
-  const [title, setTitle] = useState('Что будем строить?')
+  const [heightValue, setHeightValue] = useState(String)
+  const [materialValues, setMaterialValues] = useState(String)
 
   const dispatch = useDispatch()
-  const state = useSelector(state => state.rootReducer)
-  
-  return<>
+
+const renderItems = () => {
+  switch (step) {
+    case 1:
+        return (
+      <>
+      <div className="card-body">
+      <span className='text-muted'>{`Шаг ${step}`}</span>
+      <h5 className="card-title">Что будем строить?</h5>
+      <input type="radio" value='1' className="btn-check" name="btnradio" id='House' autoComplete="off" onChange={e => setCeckedValue(e.target.value)} />
+      <label className="card-text text-success font-weight-bold" htmlFor="House">Жилой Дом</label>
+      <br />
+      <input type="radio" value='2' className="btn-check" name="btnradio" id="Garage" autoComplete="off" onChange={e => setCeckedValue(e.target.value)} />
+      <label className="card-text text-success font-weight-bold" htmlFor="Garage">Гараж</label>
+    </div>
+    <div className="card-footer text-muted">
+      <button type="button" className="btn btn-outline-warning">Отмена</button>
+      <button type="button" className="btn btn-outline-info ml-5" onClick={() => dispatch(building(checkedValue))}>Далее</button>
+    </div>
+  </>
+  )
+  case 2:
+    return (
+        <>
+        <div className="card-body">
+        <span className='text-muted'>{`Шаг ${step}`}</span>
+        <h5 className="card-title">Колличество этажей (число):</h5>
+          <input type="number" value={heightValue} onChange={e => setHeightValue(e.target.value)}/>
+      </div>
+      <div className="card-footer text-muted">
+        <button type="button" className="btn btn-outline-warning">Отмена</button>
+        <button type="button" className="btn btn-outline-info ml-5" onClick={() => dispatch(height(heightValue))}>Далее</button>
+      </div>
+    </>
+    )
+  case 3:
+    return (
+      <>
+      <div className="card-body">
+      <span className='text-muted'>{`Шаг ${step}`}</span>
+      <h5 className="card-title">Материал Стен:</h5>
+      <input type="radio" value='1' className="btn-check" name="btnradio" id='1' autoComplete="off" onChange={e => setMaterialValues(e.target.value)} />
+      <label className="card-text text-success font-weight-bold" htmlFor="1">Кирпич</label>
+      <br />
+      <input type="radio" value='2' className="btn-check" name="btnradio" id="2" autoComplete="off" onChange={e => setMaterialValues(e.target.value)} />
+      <label className="card-text text-success font-weight-bold" htmlFor="2">Шлакоблок</label>
+      <br/>
+      <input type="radio" value='3' className="btn-check" name="btnradio" id="3" autoComplete="off" onChange={e => setMaterialValues(e.target.value)} />
+      <label className="card-text text-success font-weight-bold" htmlFor="3">Деревянный брус</label>
+    </div>
+    <div className="card-footer text-muted">
+      <button type="button" className="btn btn-outline-warning">Отмена</button>
+      <button type="button" className="btn btn-outline-info ml-5" onClick={() => dispatch(material(materialValues))}>Далее</button>
+    </div>
+  </>
+    )
+    default:
+      break;
+  }
+}
+
+  return <>
     <div className="card text-center">
       <div className="card-header">
         Калькулятор цены конструкций
      </div>
-      <div className="card-body">
-        <span className='text-muted'>{`Шаг ${step}`}</span>
-        <h5 className="card-title">{title}</h5>
-        <input type="radio" value='1' className="btn-check" name="btnradio" id='House' autoComplete="off" onChange={e => setCeckedValue(e.target.value)}/>
-        <label className="card-text text-success font-weight-bold" htmlFor="House">Жилой Дом</label>
-        <br />
-        <input type="radio" value='2' className="btn-check" name="btnradio" id="Garage" autoComplete="off" onChange={e => setCeckedValue(e.target.value)}/>
-        <label className="card-text text-success font-weight-bold" htmlFor="Garage">Гараж</label>
-      </div>
-      <div className="card-footer text-muted">
-        <button type="button" className="btn btn-outline-warning">Отмена</button>
-        <button type="button" className="btn btn-outline-info ml-5" onClick={() => dispatch(building(checkedValue))}>Далее</button>
-      </div>
+      {renderItems()}
     </div>
   </>
 }
