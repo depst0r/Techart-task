836ac494867e5f6315bd0d068597ee4dc7d43178
depst0r <depstor90@gmail.com>
2021-01-14 17:28:09 +0300

    dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { Calculator } from './Components/Calculator/Calculator'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { building } from './Redux/Actions/actions'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 export const App = () => {
+
+  const [step, setStep] = useState(1)
+  const [checkedValue, setCeckedValue] = useState(String)
+  const [title, setTitle] = useState('Что будем строить?')
+
+  const dispatch = useDispatch()
+  const state = useSelector(state => state.rootReducer)
+  
   return<>
-  <div className="container">
-    <div className="row">
-      <BrowserRouter>
-          <Route exact path='/'>
-            <Calculator />
-          </Route>
-      </BrowserRouter>
-    </div>
+    <div className="card text-center">
+      <div className="card-header">
+        Калькулятор цены конструкций
+     </div>
+      <div className="card-body">
+        <span className='text-muted'>{`Шаг ${step}`}</span>
+        <h5 className="card-title">{title}</h5>
+        <input type="radio" value='1' className="btn-check" name="btnradio" id='House' autoComplete="off" onChange={e => setCeckedValue(e.target.value)}/>
+        <label className="card-text text-success font-weight-bold" htmlFor="House">Жилой Дом</label>
+        <br />
+        <input type="radio" value='2' className="btn-check" name="btnradio" id="Garage" autoComplete="off" onChange={e => setCeckedValue(e.target.value)}/>
+        <label className="card-text text-success font-weight-bold" htmlFor="Garage">Гараж</label>
+      </div>
+      <div className="card-footer text-muted">
+        <button type="button" className="btn btn-outline-warning">Отмена</button>
+        <button type="button" className="btn btn-outline-info ml-5" onClick={() => dispatch(building(checkedValue))}>Далее</button>
+      </div>
     </div>
   </>
 }
--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -23,3 +23,27 @@ export const Calculator = () => {
         </div>
     </>
 }
+
+
+
+//app
+
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Calculator } from './Components/Calculator/Calculator'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export const App = () => {
+  return<>
+  <div className="container">
+    <div className="row">
+      <BrowserRouter>
+          <Route exact path='/'>
+            <Calculator />
+          </Route>
+      </BrowserRouter>
+    </div>
+    </div>
+  </>
+}
