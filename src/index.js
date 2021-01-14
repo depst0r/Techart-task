94697ac2b33b1a688b54485f4915f50a4066e724
depst0r <depstor90@gmail.com>
2021-01-14 13:03:04 +0300

    index

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import reducers from './Redux/Reducer/reducers'
 
-
+const state = createStore(reducers, applyMiddleware(logger, thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={state}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
